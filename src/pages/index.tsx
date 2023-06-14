301439c20c52aa88de184b48c93561eb4f3b133e
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2023-06-14 02:23:20 -0300

    Responsividade do hero inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,10 @@ export default function Home() {
       <Head>
         <title>Progno - Prognóstico judicial para advogados</title>
       </Head>
-      <div className="w-screen h-screen bg-hero flex flex-col items-center justify-center">
-        <Image src='/logo-progno-vertical.png' alt="Progno" width={400} height={400} className="-mt-8"/>
-        <div className="mt-8 text-primary-400">
-          <h1 className="text-3xl font-extralight opacity-80"><strong className="font-bold">Inteligência</strong><span> no prognóstico judicial</span></h1>
+      <div className="w-screen h-screen bg-hero flex flex-col items-center justify-center px-4">
+        <Image src='/logo-progno-vertical.png' alt="Progno" width={400} height={400} className="-mt-8 w-64 md:w-80"/>
+        <div className="mt-8 text-primary-400 text-center">
+          <h1 className="text-3xl font-extralight opacity-90"><strong className="font-bold">Inteligência</strong><span> no prognóstico judicial</span></h1>
         </div>
         <Link href={'/app'} >App</Link>
       </div>
