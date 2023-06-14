4b4f6422baf3568d6d5b35c1ae29501d6f829532
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2023-06-14 02:16:23 -0300

    Inserção do slogan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,11 @@ export default function Home() {
       <Head>
         <title>Progno - Prognóstico judicial para advogados</title>
       </Head>
-      <div className="w-screen h-screen bg-hero flex items-center justify-center">
+      <div className="w-screen h-screen bg-hero flex flex-col items-center justify-center">
         <Image src='/logo-progno-vertical.png' alt="Progno" width={400} height={400} className="-mt-8"/>
+        <div className="mt-8 text-primary-400">
+          <h1 className="text-3xl font-extralight opacity-80"><strong className="font-bold">Inteligência</strong><span> no prognóstico judicial</span></h1>
+        </div>
         <Link href={'/app'} >App</Link>
       </div>
       
